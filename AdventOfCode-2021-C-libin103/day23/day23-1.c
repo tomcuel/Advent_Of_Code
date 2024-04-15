b176fd971c76acb27fd1aacff61c422ec9b21f5a
Tom Cuel <tom.cuel3@gmail.com>
2024-04-15 16:06:38 +0200

    Day23

---
+++ AdventOfCode-2021-C-libin103/day23/day23-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h> // pour le printf
+#include <stdlib.h> // pour le scanf
+#include <math.h> // pour les fonctions mathématiques 
+#include <string.h> // pour les chaînes de caractères, strlen par exemple
+#include <stdbool.h> // pour les booléens 
+#include <time.h> // pour les randint 
+#include <limits.h> // pour les limites
+#include <ctype.h> // pour isalpha 
+#include <unistd.h> // pour getpid  
+
+int main (){
+
+    return EXIT_SUCCESS;
+}
+
+// implementation too difficult for me with too many difficulties cases 
+// seems like djikstra algorithm could be useful here, but I'm not sure how to implement it in this case
+// because it's not a classic one
---
+++ AdventOfCode-2021-C-libin103/day23/day23-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h> // pour le printf
+#include <stdlib.h> // pour le scanf
+#include <math.h> // pour les fonctions mathématiques 
+#include <string.h> // pour les chaînes de caractères, strlen par exemple
+#include <stdbool.h> // pour les booléens 
+#include <time.h> // pour les randint 
+#include <limits.h> // pour les limites
+#include <ctype.h> // pour isalpha 
+#include <unistd.h> // pour getpid  
+
+int main (){
+
+    return EXIT_SUCCESS;
+}
