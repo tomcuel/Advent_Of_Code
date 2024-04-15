f2b3ccb780ed02a13ed5cea7f85b3cc61c674edf
Tom Cuel <tom.cuel3@gmail.com>
2024-04-15 16:01:06 +0200

    Day18

---
+++ AdventOfCode-2021-C-libin103/day18/day18-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h> // pour le printf
+#include <stdlib.h> // pour le scanf
+#include <math.h> // pour les fonctions mathématiques 
+#include <string.h> // pour les chaînes de caractères, strlen par exemple
+#include <stdbool.h> // pour les booléens 
+#include <time.h> // pour les randint 
+#include <limits.h> // pour les limites
+#include <ctype.h> // pour isalpha 
+#include <unistd.h> // pour getpid  
+
+int main (){
+
+    return EXIT_SUCCESS;
+}
+
+// the input could be represented by a binary tree, the lib can give us a good one, but since we cannot repeat the same number in this tree or it will mess up the searching it's quite complicated
+// on top of that, making the additions, reductions and explosions seems really difficult 
+// that's why I didn't succeed in this challengen-, after a bit of an attempt but that wasn't successful, I decided to give up and move on to the next challenge
